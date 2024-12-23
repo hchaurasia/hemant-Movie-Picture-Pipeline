9b79a6bfbe2aa066bcb39dce21c0f83a7f1030c2
AzeematRaji <azeematjumoke@gmail.com>
2024-12-23 16:04:07 +0100

    modified front-cd

--- setup/terraform/versions.tf
+++ setup/terraform/versions.tf
@@ -3,10 +3,9 @@ provider "aws" {
 }
 
 terraform {
-  required_version = "1.3.9"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "4.55.0"
     }
   }
